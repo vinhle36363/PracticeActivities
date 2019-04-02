2c953487a0a51d473d02b101249a96d532ba36d9
Vinh Le <vle33@my.bcit.ca>
2019-04-02 12:25:58 -0700

    Add JavaScript ES6 class definition and starting JS prototype definition.

---
+++ NodejsConsoleApp1/NodejsConsoleApp1/app.js
@@ -0,0 +1,28 @@
+// JavaScript ES6 Class
+// Definition:
+//  - Special function, contain 2 components: class expression and declerations.
+class Car
+{
+    // Class's constructor
+    constructor(color, brand)
+    {
+        this.color = color;
+        this.brand = brand;
+    }
+
+    // Car details function, return a sentence of a given class value.
+    carDetails()
+    {
+        return this.color + ' ' + this.brand + ' car.';
+    }
+}
+
+// JavaScript Prototype
+// We will creating 2 obj of class car and pass in different attributes and execute carDetails()
+// Expected result: different object return diffrent results: car1 will return Black Mustang car.
+//                                                            car2 will return White Mercedes car.
+var car1 = new Car('Black', 'Mustang');
+var car2 = new Car('White', 'Mercedes');
+
+console.log(car1.carDetails()); // result: Black Mustang car.
+console.log(car2.carDetails()); // result: White Mercedes car.
